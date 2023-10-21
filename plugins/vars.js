1e7e5a951e1d3bc2b1dbf26e921ab65003534faf
inrl-official <fasweehmon2@gmail.com>
2023-10-21 17:20:27 +0530

    Update vars.js

--- plugins/vars.js
+++ plugins/vars.js
@@ -1,6 +1,4 @@
-const {
-        inrl
-} = require('../lib');
+const { inrl } = require('../lib');
 const Config = require('../config');
 inrl({
         pattern: 'getvar ?(.*)',
@@ -8,11 +6,19 @@ inrl({
         desc: 'show all config var',
         type: 'settings'
 }, async (message, match) => {
-        let msg = "*_all config vars_*\n\n";
+        let msg = "*_all config vars_*\n\n",
+                got = false;
         for (const key in Config) {
                 if (key != 'DATABASE' && key != 'BASE_URL' && key != 'HEROKU' && key != 'SESSION_ID') {
-                        msg += `_*${key}* : ${Config[key]}_\n`;
+                        if (!match) {
+                                msg += `_*${key}* : ${Config[key]}_\n`;
+                        } else if (match.toUpperCase() == key) {
+                                return await message.send(`_*${match.toUpperCase()}* : ${Config[key]}_`);
+                                got = true;
+                                break;
+                        }
                 }
         }
+        if (match && !got) return await message.send('_thet requested key not found_\n_try *getvar* to get all variables_');
         return await message.send(msg);
 });
