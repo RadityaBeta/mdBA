411c9895f764b8860c0f0f7576a403cb03c8ecf8
inrl-official <fasweehmon2@gmail.com>
2023-10-21 13:29:53 +0530

    Update and rename variable.js to vars.js

--- plugins/vars.js
+++ plugins/vars.js
@@ -11,7 +11,7 @@ inrl({
         let msg = "*_all config vars_*\n\n";
         for (const key in Config) {
                 if (key != 'DATABASE' && key != 'BASE_URL' && key != 'HEROKU' && key != 'SESSION_ID') {
-                        msg += `_*${key}* : ${a[key]}_\n`;
+                        msg += `_*${key}* : ${Config[key]}_\n`;
                 }
         }
         return await message.send(msg);
